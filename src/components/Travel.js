2a84ee6f758de565a9796969fb3ea86417a63b9f
Emil <Emilknudsen1@live.dk>
2021-01-11 11:19:02 +0100

    travel

--- src/components/Travel.js
+++ src/components/Travel.js
@@ -1,10 +1,14 @@
+import '../App.css';
+
 function Travel() {
   return (
     <div className="travel">
-        <img className="travelImg" src="./public/travel.png" alt="Travel Image"/>
+        <img className="travelImg" src="./images/travel.png" alt="Travel Image"/>
+        <div className="travelContent">
         <p className="travelCategory">Travel</p>
         <h2 className="travelHeader">Surfing in Maldives</h2>
-        <p className="travelText">It’s windy. The cool breeze of the ocean. It gives, a sense of beauty, in motion. All is flowing, rushing and tide-And I sit in wonder, dreaming beside.</p>
+        <p className="travelText">It’s windy. The cool breeze of the ocean. <br/> It gives, a sense of beauty, in motion. All <br/> is flowing, rushing and tide-And I sit in <br/> wonder, dreaming beside.</p>
+        </div>
     </div>
   );
 }
