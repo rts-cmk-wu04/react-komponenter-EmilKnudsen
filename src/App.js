74828cf70820d79e03aac408af0c53613d955d42
Emil <Emilknudsen1@live.dk>
2021-01-11 12:48:49 +0100

    add travel component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Travel from "./components/Travel";
+import Outdoor from "./components/Outdoor";
 
 function App() {
   return (
     <>
+    <div className="topSection">
     <Travel></Travel>
+    <Outdoor></Outdoor>
+    </div>
     </>
   );
 }
