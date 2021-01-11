1030b45eafef21b475bc929e13f6c09b8c63b881
Emil <Emilknudsen1@live.dk>
2021-01-11 10:50:37 +0100

    travel component

---
+++ src/components/Travel.js
@@ -0,0 +1,12 @@
+function Travel() {
+  return (
+    <div className="travel">
+        <img className="travelImg" src="./public/travel.png" alt="Travel Image"/>
+        <p className="travelCategory">Travel</p>
+        <h2 className="travelHeader">Surfing in Maldives</h2>
+        <p className="travelText">It’s windy. The cool breeze of the ocean. It gives, a sense of beauty, in motion. All is flowing, rushing and tide-And I sit in wonder, dreaming beside.</p>
+    </div>
+  );
+}
+
+export default Travel;
