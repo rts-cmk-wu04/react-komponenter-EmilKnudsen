228a412d6f9e6869c7d058ce45681120ceead265
Emil <Emilknudsen1@live.dk>
2021-01-12 12:12:49 +0100

    add components collection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Collection from "./components/Collection";
 
 function App() {
+  var [collectionContent, setCollectionContent] = useState([]);
+
+  useEffect(function() {
+    fetch("./collection.json")
+      .then(function(response) {
+        return response.json ();
+      })
+      .then (function(data) {
+        setCollectionContent(data);
+      })
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="allCollections">
+      {collectionContent.map(content => <Collection image={content.image} imgText={content.imgText} profile={content.profile} title={content.title} by={content.by} />)}
     </div>
   );
 }
-
 export default App;
