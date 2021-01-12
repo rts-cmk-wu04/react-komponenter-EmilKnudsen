7f43eca23e0cf1ade0e4bcf739b26e595a36856f
Emil <Emilknudsen1@live.dk>
2021-01-12 11:35:46 +0100

    add cards components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Cards from "./components/Cards";
 
 function App() {
+  var [cardsContent, setCardsContent] = useState([]);
+
+  useEffect(function() {
+    fetch("./data.json")
+      .then(function(response) {
+        return response.json ();
+      })
+      .then (function(data) {
+        setCardsContent(data);
+      })
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {cardsContent.map(content => <Cards image={content.image} title={content.title} text={content.text} color={content.color} />)}
     </div>
   );
 }
-
 export default App;
