89eeca26d5174002a0f1414fdf1974f8e4902055
Emil <Emilknudsen1@live.dk>
2021-01-14 16:27:07 +0100

    add digest components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Digest from "./components/Digest";
 
 function App() {
+  var [digestContent, setDigestContent] = useState([]);
+
+  useEffect(function() {
+    fetch("./digest.json")
+      .then(function(response) {
+        return response.json ();
+      })
+      .then (function(data) {
+        setDigestContent(data);
+      })
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="allDigests">
+      <div className="digestNews"><p className="digestNewsText">NEWS DIGEST</p></div>
+      <div className="digestSection">
+      {digestContent.map(content => <Digest text={content.text} title={content.title} number={content.number} color={content.color} />)}
+      </div>
     </div>
   );
 }
-
 export default App;
