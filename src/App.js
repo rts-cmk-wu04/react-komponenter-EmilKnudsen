783d06980ea1e1479887f2c1264101047af9a16e
Emil <Emilknudsen1@live.dk>
2021-01-15 09:44:03 +0100

    two cards add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Two from "./components/twoCards";
 
 function App() {
+  var [twoContent, setTwoContent] = useState([]);
+
+  useEffect(function() {
+    fetch("./twocards.json")
+      .then(function(response) {
+        return response.json ();
+      })
+      .then (function(data) {
+        setTwoContent(data);
+      })
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="allTwos">
+      {twoContent.map(content => <Two image={content.image} text={content.text} color={content.color}/>)}
     </div>
   );
 }
-
 export default App;
