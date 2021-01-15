183b90cfea2cc44df7672625479ab8090e8de7e7
Emil <Emilknudsen1@live.dk>
2021-01-15 09:16:51 +0100

    add virtual components

--- src/components/Virtual.js
+++ src/components/Virtual.js
@@ -1,1 +1,12 @@
-import "./Virtual.css"
+import './Virtual.css';
+
+function Virtual () {
+    return (
+        <div className="virtual">
+            <h2 className="virtualHeader">Virtual Reality</h2>
+            <img className="virtualImage" src="./images/virtual.png" alt="background"/>
+            <p className="virtualCategory">TECHNOLOGY</p>
+        </div>
+    );
+}
+export default Virtual;
