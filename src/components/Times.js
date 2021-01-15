a2eb803136de0a43fa134223e62b1b927eaad4ad
Emil <Emilknudsen1@live.dk>
2021-01-15 08:44:11 +0100

    times changes

--- src/components/Times.js
+++ src/components/Times.js
@@ -3,13 +3,13 @@ import "./Times.css";
 export default function Times({logo, text, color, header, link, background, textColor}) {
     return (
         <article className="times">
+        <img alt="background" className="timesBackground" src={background} alt=""/>
         <div className="timesHead">
         <img alt="logo" className="timesLogo" src={logo} />
         <h2 className="timesHeader">{header}</h2>
         </div>
         <p style={{color: textColor}} className="timesText">{text}</p>
         <a style={{color: color}} className="timesLink" href="#">{link}</a>
-        <img alt="background" className="timesBackground" src={background} alt=""/>
         </article>
     )
 }
